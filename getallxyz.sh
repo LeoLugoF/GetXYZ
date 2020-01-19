9cabe8c73710d414c08affb5967ff736a20b5182
Leo Lugo <51517644+LeoLugoF@users.noreply.github.com>
2020-01-19 16:12:13 -0600

    Uploaded files

---
+++ getallxyz.sh
@@ -0,0 +1,52 @@
+#!/bin/bash
+
+if [ -z "$1" ];
+then
+	echo "Insert the name of the file where all the info is going to be storaged after the script name."
+	echo "This script extracts all the xyz coordinates from gaussian output with extension *.out and *.log"
+	echo "From the current directory and subdirectories."
+else
+
+if [ -f $1 ];
+then
+	rm $1
+fi
+
+NFilesFoundlog=$(find $directory -name "*.log" | wc -l)
+NFilesFoundOut=$(find $directory -name "*.out" | wc -l)
+NFilesFound=$(($NFilesFoundlog+$NFilesFoundOut))
+
+i=1
+
+echo "Number of files found (*.out,*.log): $NFilesFound"
+
+for file in $(find $directory -type f -name "*.log" && find $directory -type f -name "*.out");
+do 
+filenamext=$(basename $file)
+
+echo -n "($i/$NFilesFound) extracting from file: $file"
+
+awk -F " " 'NF==6' $file | tac | awk '/Rotational/{p=1} p; /Number/{exit}' | tac | awk '/Number/{p=1} p; /Rotational/{exit}' | tail -n +2 | tac | tail -n +2 | tac | awk '{print $2 " " $4 " " $5 " " $6}' > "1$1"
+
+i=$(($i+1))
+
+if [ "$(wc -w "1$1")" == 0 ];
+	then
+	echo "	No coordinates found"
+	rm "1$1"	
+	else
+	echo "${filenamext%%.*}" >> "$1"
+	echo " " >> "$1"
+	awk '{print $0}' 1$1 >> "$1"
+	echo " " >> "$1"
+	finaline=$(tail -1 $1)
+	echo -n "	Done! "
+	if [[ $finaline == *"Normal"* ]];
+	then
+		echo "	Normal Termination."
+		else
+		echo "	Not terminated."
+	fi
+	fi
+done
+fi
---
+++ getxyz.sh
@@ -0,0 +1,52 @@
+#!/bin/bash
+
+declare -A ptable
+#ptable['key']='value'
+ptable=( ["1"]="H" ["2"]="He" ["3"]="Li" ["4"]="Be" ["5"]="B" ["6"]="C" ["7"]="N" ["8"]="O" 
+ ["9"]="F" ["10"]="Ne" ["11"]="Na" ["12"]="Mg" ["13"]="Al" ["14"]="Si" ["15"]="P" ["16"]="S" 
+ ["17"]="Cl" ["18"]="Ar" ["19"]="K" ["20"]="Ca" ["21"]="Sc" ["22"]="Ti" ["23"]="V" ["24"]="Cr"
+ ["25"]="Mn" ["26"]="Fe" ["27"]="Co" ["28"]="Ni" ["29"]="Cu" ["30"]="Zn" ["31"]="Ga" ["32"]="Ge"
+ ["33"]="As" ["34"]="Se" ["35"]="Br" ["36"]="Kr" ["37"]="Rb" ["38"]="Sr" ["39"]="Y" ["40"]="Zr"
+ ["41"]="Nb" ["42"]="Mo" ["43"]="Tc" ["44"]="Ru" ["45"]="Rh" ["46"]="Pd" ["47"]="Ag" ["48"]="Cd"
+ ["49"]="In" ["50"]="Sn" ["51"]="Sb" ["52"]="Te" ["53"]="I" ["54"]="Xe" ["55"]="Cs" ["56"]="Ba" 
+ ["71"]="Lu" ["72"]="Hf" ["73"]="Ta" ["74"]="W" ["75"]="Re" ["76"]="Os" ["77"]="Ir" ["78"]="Pt"
+ ["79"]="Au" ["80"]="Hg" ["81"]="Tl" ["82"]="Pb" ["83"]="Bi" ["84"]="Po" ["85"]="At" ["86"]="Rn" )
+
+ if [ -z "$1" ];
+ then
+	echo "Insert the name of the gaussian output after the name of the script."
+	echo "This script extracts the xyz coordinates from a gaussian output"
+        echo "From the current directory and subdirectories."
+ else
+
+awk -F " " 'NF==6' $1 | tac | awk '/Rotational/{p=1} p; /Number/{exit}' | tac | awk '/Number/{p=1} p; /Rotational/{exit}' | tail -n +2 | tac | tail -n +2 | tac | awk '{print $2 " " $4 " " $5 " " $6}' > "${1%%.*}1.xyz"
+
+if [ "$(wc -w ${1%%.*}1.xyz)" == 0 ];
+ then
+	 echo "No coordinates found"
+	 rm "${1%%.*}1.xyz"
+ else
+
+echo "${1%%.*}" > "${1%%.*}.xyz"
+
+while IFS= read -r line
+do
+	an=$(echo "$line" | awk '{print $1}') 
+	as="${ptable[$an]}"
+	Info=$(echo "$line" | awk '{print $1=as " " $2 " " $3 " " $4}' as="$as")
+	echo $Info >> "${1%%.*}.xyz"
+done < "${1%%.*}1.xyz"
+
+rm "${1%%.*}1.xyz"
+
+finaline=$(tail -1 $1)
+
+if [[ $finaline == *"Normal"* ]];
+then
+	echo "Normal Termination."
+else
+	echo "Not terminated."
+fi
+
+fi
+fi
