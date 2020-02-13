c0ce7369e435c2ccaa3ad37c76a6f638c38f58db
Leo Lugo <51517644+LeoLugoF@users.noreply.github.com>
2020-02-13 16:18:12 -0600

    Update getallxyz.sh

--- getallxyz.sh
+++ getallxyz.sh
@@ -27,60 +27,64 @@ then
 	echo "From the current directory and subdirectories."
 else
 
-if [ -f $1 ];
-then
-	rm $1
-fi
-
-NFilesFoundlog=$(find $directory -name "*.log" | wc -l)
-NFilesFoundOut=$(find $directory -name "*.out" | wc -l)
-NFilesFound=$(($NFilesFoundlog+$NFilesFoundOut))
-
-i=1
-
-echo "Number of files found (*.out,*.log): $NFilesFound"
-
-for file in $(find $directory -type f -name "*.log" | sort -V);
-do 
-filenamext=$(basename $file)
-
-echo -n "($i/$NFilesFound) extracting from file: $file"
-
-awk -F " " 'NF==6' $file | tac | awk '/Rotational/{p=1} p; /Number/{exit}' | tac | awk '/Number/{p=1} p; /Rotational/{exit}' | tail -n +2 | tac | tail -n +2 | tac | awk '{print $2 " " $4 " " $5 " " $6}' > "1$1"
-
-i=$(($i+1))
-
-if [ "$(wc -w "1$1" | awk '{print $1}')" == 0 ];
+	if [ -f $1 ];
 	then
-	echo "	No coordinates found"
-	else
-	echo "${filenamext%%.*}" >> "$1"
-
-	SCFenergy=$(grep "extrapolated energy" $file | tail -1)
-	if [ "$2" == '-e' ]; then
-	if [[ $SCFenergy == *"extrapolated"* ]];
-	then
-		SCFenergy=$(grep "extrapolated energy" $file | tail -1 | awk '{print $5}')
-		echo "ONIOM energy = $SCFenergy" >> "$1"
-	else
-		SCFenergy=$(grep "SCF Done" $file | tail -1 | awk '{print $5}')
-		echo "SCF energy = $SCFenergy" >> "$1"
-	fi
+		rm $1
 	fi
 
-	echo " " >> "$1"
-	awk '{print $0}' 1$1 >> "$1"
-	echo " " >> "$1"
-	finaline=$(tail -1 $file)
-	echo -n "	Done! "
-	if [[ $finaline == *"Normal"* ]];
-	then
-		echo "	Normal Termination."
+	NFilesFoundlog=$(find $directory -name "*.log" | wc -l)
+	NFilesFoundOut=$(find $directory -name "*.out" | wc -l)
+	NFilesFound=$(($NFilesFoundlog+$NFilesFoundOut))
+
+	i=1
+
+	echo "Number of files found (*.out,*.log): $NFilesFound"
+
+	for file in $(find $directory -type f -name "*.log" | sort -V);
+	do 
+		filenamext=$(basename $file)
+
+		echo -n "$(tput sgr0)($i/$NFilesFound) extracting from file: $file"
+
+		awk -F " " 'NF==6' $file | tac | awk '/Rotational/{p=1} p; /Number/{exit}' | tac | awk '/Number/{p=1} p; /Rotational/{exit}' | tail -n +2 | tac | tail -n +2 | tac | awk -v OFS="    " '{print $2, $4, $5, $6}' > "1$1"
+		
+		#Gets right the columns for negative signs
+		sed -i 's/ -/-/g' 1$1
+		#Deletes one space at the beggining of the line of each line in file for Atomic number above two numbers.
+		sed -i 's/^\([0-9][0-9]\) \( \)/\1\2/' 1$1
+		i=$(($i+1))
+		
+		if [ "$(wc -w "1$1" | awk '{print $1}')" == 0 ];
+		then
+			echo "$(tput setaf 1) No coordinates found"
 		else
-		echo "	Not terminated."
-	fi
-	fi
-rm "1$1"
-done
+
+			echo "${filenamext%%.*}" >> "$1"
+
+			SCFenergy=$(grep "extrapolated energy" $file | tail -1)
+			if [ "$2" == '-e' ]; then
+				if [[ $SCFenergy == *"extrapolated"* ]];
+				then
+					SCFenergy=$(grep "extrapolated energy" $file | tail -1 | awk '{print $5}')
+					echo "SCF energy = $SCFenergy" >> "$1"
+				else
+					SCFenergy=$(grep "SCF Done" $file | tail -1 | awk '{print $5}')
+					echo "SCF energy = $SCFenergy" >> "$1"
+				fi
+			fi
+			echo " " >> "$1"
+			awk '{print $0}' 1$1 >> "$1"
+			echo " " >> "$1"
+			finaline=$(tail -1 $file)
+			echo -n "$(tput setaf 2)	Done! "
+			if [[ $finaline == *"Normal"* ]];
+			then
+				echo "$(tput setaf 2)	Normal Termination."
+			else
+				echo "$(tput setaf 1)	Not terminated."
+			fi
+		fi
+		rm "1$1"
+	done
 fi
 
